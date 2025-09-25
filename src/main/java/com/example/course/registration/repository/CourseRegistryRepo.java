508f8420c2c5bc74eb774724c9d34ad58ace992d
balajimithun000 <balajimithun000@gmail.com>
2025-09-25 15:25:47 +0530

    Clean root folder, keep only backend folder

--- src/main/java/com/example/course/registration/repository/CourseRegistryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.course.registration.repository;
-
-import com.example.course.registration.model.CourseRegistry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRegistryRepo extends JpaRepository<CourseRegistry, Integer> {
-}
--- src/main/java/com/example/course/registration/repository/CourseRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.course.registration.repository;
-
-import com.example.course.registration.model.course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepo extends JpaRepository<course, String> {
-}
